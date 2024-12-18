f7687a8e222a1928e6b4004b3b7dcbb33ff92db0
Sivia <87316008+sivia1@users.noreply.github.com>
2024-12-18 19:23:36 +0530

    Time: 4 ms (71.15%), Space: 20.2 MB (28.57%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> adj(n);
+        for(auto pair : prerequisites) {
+            adj[pair[1]].push_back(pair[0]);
+        }
+        //now perform topological sorting
+
+        vector<int> indegree(n, 0);
+        for(int i = 0; i < n; i++) {
+            for(auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it : adj[node]) {
+                indegree[it]--;
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size() == numCourses) {
+            return true;
+        }
+        return false;
+    }
+};
