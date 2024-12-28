b4cbcc85b17e1eb3c4b1833f01452b94aa326c33
Sivia <87316008+sivia1@users.noreply.github.com>
2024-12-28 11:39:09 +0530

    Time: 0 ms (100%), Space: 8.1 MB (14.96%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.cpp
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
+//without using + operator; bit manipulation
     int sum(int num1, int num2) {
-        return num1+num2;
+        while(num2 != 0) {
+            int carry = num1 & num2; //calculate carry
+            num1 = num1 ^ num2; // add without carry
+            num2 = carry << 1; //shift carry to the left
+        }
+        return num1;
     }
 };
