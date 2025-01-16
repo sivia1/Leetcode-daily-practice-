9778e33aec682b0ed0ddf8f3b26c8f442fb0663d
Sivia <87316008+sivia1@users.noreply.github.com>
2025-01-16 21:52:20 +0530

    Time: 13 ms (35.87%), Space: 42.2 MB (5.75%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<pair< int, int>> diff;
+        for(int num : arr) {
+            diff.push_back({abs(num - x), num});
+        }
+        sort(diff.begin(), diff.end(), [](pair<int, int> &a, pair<int, int> &b) {
+            if(a.first == b.first) {
+                return a.second < b.second;
+            }
+            return a.first < b.first;
+        });
+        vector<int> result;
+        for(int i = 0; i < k; i++) {
+            result.push_back(diff[i].second);
+        }
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
