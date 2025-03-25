f2924b74d7a060d84ce9f027887c01214ea3fb88
Sivia <87316008+sivia1@users.noreply.github.com>
2025-03-25 17:20:02 +0530

    Time: 30 ms (38.68%), Space: 16.2 MB (77.06%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,30 +3,33 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int count = 0;
-        int i, j;
+        if(m == 0) {
+            return 0;
+        }
+        int numOfIslands = 0;
 
-        for(i = 0; i < m; i++) {
-            for(j = 0; j < n; j++) {
-
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(grid[i][j] == '1') {
                     dfs(grid, i, j, m, n);
-                    count++;
+                    numOfIslands++;
                 }
             }
         }
-        return count;
+        return numOfIslands;
     }
 
+private:
     void dfs(vector<vector<char>>& grid, int i, int j, int m, int n) {
-        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
+        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
             return;
         }
+
         grid[i][j] = '0';
 
-        dfs(grid, i+1, j, m, n);
         dfs(grid, i-1, j, m, n);
-        dfs(grid, i, j+1, m, n);
+        dfs(grid, i+1, j, m, n);
         dfs(grid, i, j-1, m, n);
+        dfs(grid, i, j+1, m, n);
     }
 };
