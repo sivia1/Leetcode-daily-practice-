20dcb0efe361ec932015f323a4c41f0988f4c4ae
Sivia <87316008+sivia1@users.noreply.github.com>
2025-04-12 21:41:43 +0530

    Time: 3 ms (77.9%), Space: 11.5 MB (9.78%) - LeetHub

--- 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.cpp
+++ 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+//idea : count all the ways to choose 3 different numbers and pick one index for each such that indices are strictly increasing
+
+//fix the middle element cnt elements in the current group
+// there are left elements from before cnt 
+//there are right elements after cnt 
+
+// The number of distinct triplets where:
+// One element is from before group i
+// One is from group i (as the middle)
+// One is from after group i is-->
+//left * cnt * right (one from each group)
+// This formula naturally avoids duplicates, respects index order i < j < k, and ensures values are distinct.
+
     int unequalTriplets(vector<int>& nums) {
-        int n = nums.size();
-        int count = 0;
-        for(int i = 0; i < n-2; i++) {
-            for(int j = i+1; j < n-1; j++) {
-                for(int k = j+1; k< n; k++) {
-                    if((nums[i] != nums[j]) && (nums[i] != nums[k]) && (nums[j] != nums[k]) && (i < j < k)) {
-                        count++;
-                    }
-                }
-            }
+        unordered_map<int, int> freq;
+        for(int num : nums) {
+            freq[num]++;
         }
-        return count;
+        int n = nums.size();
+        int res = 0;
+        int left = 0;
+        for(auto [val, cnt] : freq) {
+            int right = n - left - cnt;
+            res += left * cnt * right;
+            left += cnt;
+        }
+        return res;
     }
 };
