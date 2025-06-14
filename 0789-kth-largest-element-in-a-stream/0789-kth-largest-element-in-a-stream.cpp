df3be269abf570b0ac73076adfdf90352ca6f4a8
Sivia <87316008+sivia1@users.noreply.github.com>
2025-06-14 11:36:53 +0530

    Time: 4 ms (82.85%), Space: 33.1 MB (60.47%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,40 @@
+class KthLargest {
+//approach :
+    //builind a live ranking system that takes input and displays the kth largest element
+    //keeps track of the kth highest score
+    //updates ranking every time a new student submits score
+
+    //USE MIN-HEAP
+    //Only the top K largest elements matter
+    //Among these elements, smallest one is our Kth Largest
+    //A min-Heap of size k gives us smallest element at top
+private:
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int key;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        key = k;
+        for(int num : nums) {
+            minHeap.push(num);
+
+            if(minHeap.size() > key) {
+                minHeap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+
+        if(minHeap.size() > key) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
