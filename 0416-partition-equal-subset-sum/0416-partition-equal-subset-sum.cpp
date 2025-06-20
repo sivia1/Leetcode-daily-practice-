53d7e3facad71578dbe4757c5e8c7cd0b3aa62b7
Sivia <87316008+sivia1@users.noreply.github.com>
2025-06-20 15:42:20 +0530

    Time: 106 ms (68.82%), Space: 13.3 MB (98.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//Approach Tabulation (2D dp): bool matrix of size [n+1][sum+1]; dp[n+1][sum+1]
+//Approach Tabulation (1D dp): Space Optimized
     bool canPartition(vector<int>& nums) {
         //if the sum of array is even, then only the subset can be equally divided. If sum not even return false
         // pattern if sum is even, target = sum/2 and problem is reduced to subset sum problem, do it in Recursion, Memoization, Tabulation, Space Optimized
@@ -9,25 +9,20 @@ public:
         if(sum%2 != 0) {
             return false;
         }
-        sum % 2 == 0;
         int target = sum/2;
-        vector<vector<bool>> dp(n+1, vector<bool>(sum+1, false));
+        vector<bool> dp(sum+1, false);
 
         for(int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+            dp[0] = true;
         }
-
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= target; j++) {
-                //don't pick the element
-                dp[i][j] = dp[i-1][j];
-
-                //pick if possible, previous element is < sum
-                if(nums[i-1] <= j) {
-                    dp[i][j] = dp[i][j] || dp[i-1][j-nums[i-1]];
-                }
+        //process each number
+        for(int num : nums) {
+            //traverse right to left to avoid using updated values
+            for(int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j-num];
             }
+            
         }
-        return dp[n][target];
+        return dp[target];
     }
 };
