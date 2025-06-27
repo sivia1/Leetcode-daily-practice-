54ce3fbea4df03551682785fed2ed6f634ef97f9
Sivia <87316008+sivia1@users.noreply.github.com>
2025-06-27 17:05:01 +0530

    Time: 4 ms (86.04%), Space: 26.2 MB (47.06%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -9,14 +9,15 @@ public:
             int mid = left + (right - left) / 2;
             long long sum = 0;
             for(int num : nums) {
+                //correct ceiling formula
                 sum += (num-1)/mid+1;
             }
 
-            if(sum <= threshold) { //sum is < threshold so gotta check if there's more valid answer
+            if(sum <= threshold) { //sum is < threshold so gotta try smaller divisor (denominator < numerator = larger quotient)
                 result = mid;
                 right = mid - 1;
                 
-            } else { // sum > threshold so gotta check towards left
+            } else { // sum > threshold so gotta check towards the right half of the array 
                 left = mid + 1;
             }
         }
