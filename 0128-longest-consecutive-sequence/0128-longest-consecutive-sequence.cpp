6b9385e5accd768d9b7bc339e714f9aaf9eb0c35
Sivia <87316008+sivia1@users.noreply.github.com>
2025-07-06 19:04:04 +0530

    Time: 95 ms (26.33%), Space: 89 MB (28.94%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-//for consecutive elements you gotta check if(nums[i+1] ==. nums[i]+1)
+// METHOD - 1 for consecutive elements you gotta check if(nums[i+1] ==. nums[i]+1)
 //if duplicates then skip use continue;
 //if in sequence i.e if nums[i] == nums[i-1]+1 then increment currLength 
 //if not then reset curr length to 1 and store the previously occurring currlength to maxLength
+
+//METHOD -2 using HashSet for Order 1 lookup.. you store the array in the set and then you traverse the set and check if is the beginning of a sequence by looking for num-1 number in the set 
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
         if(nums.empty()) {
             return 0;
         }
-        sort(nums.begin(), nums.end());
-
+        //store all numbers in hash-set for O(1) look up
+        unordered_set<int> Map(nums.begin(), nums.end());
         int maxLength = 1;
-        int currLength = 1;
 
-        for(int i = 1; i < n; i++) {
-            
-            if(nums[i] == nums[i-1]) {
-                continue;
-            } else if(nums[i] == nums[i-1]+1) {
-                currLength++;
-            } else {
+        for(int num : Map) {
+            //start counting only if this number is the beginning of a sequence
+            //i.e num-1 doesn't exiat in the set 
+            if(Map.find(num-1) == Map.end()) {
+                int currNum = num;
+                int currLength = 1;
+
+                //count consecutive numbers
+                while(Map.find(currNum+1) != Map.end()) {
+                    currNum++;
+                    currLength++;
+                }
                 maxLength = max(maxLength, currLength);
-                currLength = 1;
             }
         }
-        return max(maxLength, currLength);
+        return maxLength;
     }
 };
