e3d07e6b671cbfbd8bada5ec3bb44800a5bbcc9a
Sivia <87316008+sivia1@users.noreply.github.com>
2025-07-09 21:24:45 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
+//to check the validity if two strings are anangrams or not.. 
+// 1) We need to see if their sizes are equal or not
+// 2) We need to count the freq of each letter of string s and simultaneously decrement the freq of each letter of t from the same hashmap where we store the freq of each letter
+//then we traverse through the hashmap and see if for every letter the freq = 0 if it is then return true other false; 
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) {
             return false;
         }
-        unordered_map<char, int> freq;
+        unordered_map<int, int> freq;
         for(int i = 0; i < s.size(); i++) {
             freq[s[i]]++;
             freq[t[i]]--;
