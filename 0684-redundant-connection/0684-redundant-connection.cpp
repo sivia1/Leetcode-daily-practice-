82ee9389ae16b437e40c024cbbe42d97b792d852
Sivia <87316008+sivia1@users.noreply.github.com>
2025-09-08 22:24:23 +0530

    Time: 0 ms (100%), Space: 14.4 MB (20.17%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+//cycle prevention and not detection
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<vector<int>> adj(n+1);
+
+        for(const auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            vector<bool> visit(n+1, false);
+
+            if(dfs(u, -1, adj, visit)) {
+                return {u, v};
+            }
+        }
+        return {};
+    }
+private:
+    bool dfs(int node, int parent, vector<vector<int>>& adj, vector<bool>& visit) {
+        if(visit[node]) {
+            return true;
+        }
+        visit[node] = true;
+        for(int neigh : adj[node]) {
+            if(neigh == parent) {
+                continue;
+            }
+            if(dfs(neigh, node, adj, visit)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
