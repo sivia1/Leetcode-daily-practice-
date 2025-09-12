29f03ca0bfc82863785e7f1176f21f26581ddfcd
Sivia <87316008+sivia1@users.noreply.github.com>
2025-09-12 22:41:56 +0530

    Time: 11 ms (41.17%), Space: 57.9 MB (12.56%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,33 +1,48 @@
 class Twitter {
-    int time;
-    unordered_map<int, unordered_set<int>> followMap;
-    unordered_map<int, vector<pair<int, int>>> tweetMap;
+    int count;
+    unordered_map<int, set<int>> followMap;
+    unordered_map<int, vector<vector<int>>> tweetMap;
 public:
-    Twitter() : time(0) {}
-    
-    void postTweet(int userId, int tweetId) {
-        tweetMap[userId].push_back({time++, tweetId});
+    Twitter() {
+        count = 0;
     }
     
+    void postTweet(int userId, int tweetId) {
+        tweetMap[userId].push_back({count++, tweetId});
+    }
+    // looks like a priority queue would be a pretty great idea
+    //also need to keep track of time the tweet was posted from the particular tweetID
     vector<int> getNewsFeed(int userId) {
-        vector<pair<int, int>> feed = tweetMap[userId];
-        for(int followeeId : followMap[userId]) {
-            feed.insert(feed.end(), tweetMap[followeeId].begin(), tweetMap[followeeId].end());
-        }
-        sort(feed.begin(), feed.end(), [](auto &a, auto &b) {
-            return a.first > b.first;
-        });
         vector<int> res;
-        for (int i = 0; i < min(10, (int)feed.size()); ++i) {
-            res.push_back(feed[i].second);
+        auto compare = [] (const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(compare)> minHeap(compare);
+        followMap[userId].insert(userId);
+        for(int followeeId : followMap[userId]) {
+            if(tweetMap.count(followeeId)) {
+                const vector<vector<int>>& tweets = tweetMap[followeeId];
+                int index = tweets.size() - 1;
+                minHeap.push({tweets[index][0], tweets[index][1], followeeId, index});
+            }
+        }
+        while(!minHeap.empty() && res.size() < 10) {
+            vector<int> curr = minHeap.top();
+            minHeap.pop();
+            res.push_back(curr[1]);
+            int index = curr[3];
+            if (index > 0) {
+                const vector<int>& tweet = tweetMap[curr[2]][index - 1];
+                minHeap.push({tweet[0], tweet[1], curr[2], index - 1});
+            }
         }
         return res;
     }
+    //For the below two functions, we can use the data structure hashmap -> userID --> Hash Set of followee id
+    //HashMap --> userId --> list of [count, TweetId]
     
     void follow(int followerId, int followeeId) {
-        if (followerId != followeeId) {
-            followMap[followerId].insert(followeeId);
-        }
+        followMap[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
