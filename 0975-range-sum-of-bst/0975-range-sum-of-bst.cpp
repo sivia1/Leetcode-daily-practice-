cab05c0dda6f5bb1ffd6fe4884f5eaa6b73488c0
Sivia <87316008+sivia1@users.noreply.github.com>
2025-10-28 11:30:31 +0530

    Time: 86 ms (4.08%), Space: 65 MB (62.29%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
@@ -10,33 +10,43 @@
  * };
  */
 class Solution {
-    //initial idea is to traverse inorder store the nodes in the array and find the sum b/w low and high
 public:
+//Approach --> leverage the BST property and prune entire subtrees that cannot possibly contain nodes within the desired range [low, high]
+//visit the nodes only in the relevant part of the tree
+//start at root and maintain a running sum
+//calculate the curr node's values (node->val):
+    // if low <= node->val <= high, add node->val to the sum.
+    // Decide which subtrees to traverse:
+        //Left Subtree --> if node->val > low it's possible that values in the left subtree could still be >= low, so traverse the left child.
+        //If node->val < low then all values in the left subtree will also be < low, so you can skip the left subtree 
+
+        //Right Subtree --> if node-> val < high it's possible that values in the right subtree(which are larger) could still be <= high, so you must traverse the right child.
+        //If node->val > high then all values in the right subtree will also be > high, so you can skip the right subtree
     int rangeSumBST(TreeNode* root, int low, int high) {
-        vector<int> sortedValues;
-
-        inOrder(root, sortedValues);
-        int totalSum = 0;
-
-        for(int val : sortedValues) {
-            if(val >= low && val <= high) {
-                totalSum += val;
-            } else if (val > high) {
-                break;
-            }
-        }
-        return totalSum;
-    }
-
-    void inOrder(TreeNode* node, vector<int>& store) {
-        if(node == nullptr) {
-            return;
+        //base case
+        if(root == nullptr) {
+            return 0;
         }
 
-        inOrder(node->left, store);
-        store.push_back(node->val);
-        inOrder(node->right, store);
-    }
+        int currSum = 0;
+        if(root->val >= low && root->val <= high) {
+            currSum += root->val;
+        }
+        // 2. Decide on the left traversal (Pruning)
+        // If the current node's value is > low, the left child MIGHT still be >= low.
+        // We only traverse left if it's possible for the left subtree to contain relevant nodes
+        if(root->val > low) {
+            currSum += rangeSumBST(root->left, low, high);
+        }
 
-    //This is correct but less optimal because it visits every single node in the tree and uses O(N) extra space to store the vector, regardless of how small the required range is.
+        // 3. Decide on the right traversal (Pruning)
+        // If the current node's value is < high, the right child MIGHT still be <= high.
+        // We only traverse right if it's possible for the right subtree to contain relevant nodes
+        if(root->val < high) {
+            currSum += rangeSumBST(root->right, low, high);
+       }
+       return currSum;
+    }
 };
+
+//This pruning technique ensures you only visit the nodes in the relevant part of the tree, potentially reducing the time complexity from O(N) to O(K + H) (where K is the number of nodes in the range and H is the tree height, as you might traverse down to find the range boundaries), and the space complexity (due to recursion stack) is $O(H)$, which is more efficient than O(N) space.
